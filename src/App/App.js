c6a2502330f345c67239dd3dccfb74f1f03bd907
andrew <andrewjthomsen@yahoo.com>
2019-11-02 10:53:22 -0700

    Created Pokegame component and added in component's main function which is to deal out cards to hand1 and hand 2, utilizes a while loop

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import Pokedex from "../Pokedex/Pokedex";
+import Pokegame from "../Pokegame/Pokegame";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">
-      <Pokedex />
+    return (
+    <div className="App">
+      <Pokegame />
     </div>
+    )
   }
 }
 
--- src/Pokegame/Pokegame.js
+++ src/Pokegame/Pokegame.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 
 
-class Pokegame extends React.Component {
+class Pokegame extends Component {
+    static defaultProps = {
+        pokemon: [
+          { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+          { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+          { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+          { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+          { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+          { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+          { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+          { id: 133, name: "Eevee", type: "normal", base_experience: 65 }
+        ]
+      };
     render() {
+        // Goal of this is to randomly pick pokemon from player2 hand and move it to player1 hand. Eventually will result in both hands being equal lengths.
+        let hand1 = [];
+        let hand2 = [...this.props.pokemon];
+        while (hand1.length < hand2.length) {
+            let randomIndex = Math.floor(Math.random() * hand2.length);
+            let randomPokemon = hand2.splice(randomIndex, 1)[0];
+            hand1.push(randomPokemon);
+        }
+        console.log(hand1);
+        console.log(hand2);
         return (
             <div>
                 <h1>Pokegame</h1>
