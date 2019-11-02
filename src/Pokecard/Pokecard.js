f7c320896da7b9180e5ae54fe4b93406489c24dc
andrew <andrewjthomsen@yahoo.com>
2019-11-02 10:15:38 -0700

    Finished adding bare-bones styling and return better quality images for Pokemon. Next am building a new component which will be called Pokegame

--- src/Pokecard/Pokecard.js
+++ src/Pokecard/Pokecard.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
 import "./Pokecard.css";
 
+// const Pokemon_API =
+//   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
 const Pokemon_API =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+  "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+// Function that takes a number in, determines if its less than or equal to 999,
+// If number is greater then just return number, if less pad with two zeros plus the number
+// Last array slice method is applied to slice 3 digits
+  let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3): number)
 
 class Pokecard extends Component {
   render() {
-    let imgSrc = `${Pokemon_API}${this.props.id}.png`;
+    let imgSrc = `${Pokemon_API}${padToThree(this.props.id)}.png`;
     return (
       <div className="Pokecard">
-        <h1>{this.props.name}</h1>
+        <h1 className="Pokecard-title">{this.props.name}</h1>
         <img src ={imgSrc} alt={this.props.name}/>
-        <div>Type: {this.props.type}</div>
-        <div>EXP: {this.props.base_experience}</div>
+        <div className="Pokecard-data">Type: {this.props.type}</div>
+        <div className="Pokecard-data">EXP: {this.props.base_experience}</div>
       </div>
     );
   }
