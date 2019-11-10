8a4d4a499a251047462377cfe5c68cd7ec614cec
andrew <andrewjthomsen@yahoo.com>
2019-11-09 21:03:31 -0800

    Psuedo coded next steps for app

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import Pokegame from "../Pokegame/Pokegame";
 import "./App.css";
-
+// Possible goals for Pokedex:
+// Integrate state
+// Further style
+// Figure out to possible add redux
+// Instead of hard coded pokemon, have a random pokemon generateor
 class App extends Component {
   render() {
     return (
